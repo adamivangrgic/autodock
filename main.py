7b4da82e3d9d43bd667e683659b1114237966b6b
adamivangrgic <grgic.ivan.adam@gmail.com>
2025-10-25 16:06:27 +0200

    added logging

--- main.py
+++ main.py
@@ -47,27 +47,34 @@ def write_json_file(file_path: str, data):
 async def git_check(name: str, url: str, branch: str, build_command: str, deploy_command: str):
     global repo_data
 
+    print(f"TASK ({name}) : running git check task")
+
     if name not in repo_data:
         repo_data[name] = {'latest_hash': None}
     
     latest_hash = repo_data[name]['latest_hash']
     new_hash = get_remote_hash(url, branch)
 
-    if latest_hash == new_hash:             # stop process if hashes are identical
+    if latest_hash == new_hash:                                                                             # stop process if hashes are identical
+        print(f"TASK ({name}) : hashes identical, aborting task.")
         return None
     
     repo_dir = os.path.join(REPO_DATA_PATH, name)
     
-    if latest_hash == None:                 # never cloned, clone entire repo
+    if latest_hash == None:                                                                                 # never cloned, clone entire repo
+        print(f"TASK ({name}) : first instance, cloning repository.")
         clone_repo(url, repo_dir, branch)
     else:
-        pull_repo(repo_dir)                 # otherwise pull changes
+        print(f"TASK ({name}) : pulling repository.")
+        pull_repo(repo_dir)                                                                                 # otherwise pull changes
     
+    print(f"TASK ({name}) : executing build command.")
     run_command(build_command, repo_dir)
     
+    print(f"TASK ({name}) : executing deploy command.")
     run_command(deploy_command, repo_dir)
 
-    repo_data[name] = {'latest_hash': new_hash}         # update to new hash after a successful build and deploy process
+    repo_data[name] = {'latest_hash': new_hash}                                                             # update to new hash after a successful build and deploy process
     write_json_file(REPO_DATA_FILE_PATH, repo_data)
 
 
@@ -76,18 +83,20 @@ async def startup_event():
     global repo_data
 
     config_data = read_yaml_file(CONFIG_FILE_PATH)
+    if not config_data:                                                                                     # stop startup if no config
+        print(f"STARTUP: {CONFIG_FILE_PATH} doesn't exist, aborting startup")
+        return None
 
-    if not config_data:                     # stop startup if no config
+    if len(config_data['repos']) == 0:  
+        print(f"STARTUP: no repositories found in {CONFIG_FILE_PATH}, aborting startup")                    # stop startup if no repos in json
         return None
 
     repo_data = read_json_file(REPO_DATA_FILE_PATH)
 
     if not repo_data:
+        print(f"STARTUP: writing empty repo data file to {REPO_DATA_FILE_PATH}")
         repo_data = {}
         write_json_file(REPO_DATA_FILE_PATH, repo_data)
-
-    if len(config_data['repos']) == 0:      # stop startup if no repos in json
-        return None
 
     scheduler = AsyncIOScheduler()
 
@@ -105,8 +114,11 @@ async def startup_event():
             id="git_check_periodic_task_" + repo['name'],
             replace_existing=True
         )
+
+        print(f"STARTUP: scheduler task configured for {repo['name']}, interval {repo['interval']} seconds")
     
     scheduler.start()
+    print("STARTUP: scheduler started")
 
 # # Define a root endpoint
 # @app.get("/")
--- subprocess_functions.py
+++ subprocess_functions.py
@@ -2,6 +2,7 @@ import os, subprocess
 
 
 def get_remote_hash(repo_url, branch='main'):
+    print(f"SUBPROCESS:  Getting {repo_url} {branch} hash")
     result = subprocess.check_output(
         ["git", "ls-remote", repo_url, f"refs/heads/{branch}"],
         text=True
@@ -9,24 +10,28 @@ def get_remote_hash(repo_url, branch='main'):
     return result.split()[0] if result else None
 
 def clone_repo(repo_url, repo_dir, branch="main"):
+    print(f"SUBPROCESS:  Cloning into repo {repo_url} {branch}")
     if not os.path.exists(os.path.join(repo_dir, ".git")):
         subprocess.run(
             ["git", "clone", "--branch", branch, "--single-branch", repo_url, repo_dir],
             check=True
         )
+        print("SUBPROCESS:  Repo successfully cloned.")
     else:
-        print("Repository already exists.")
+        print("SUBPROCESS:  Repository already exists.")
 
 def pull_repo(repo_dir):
+    print(f"SUBPROCESS:  Pulling repo {repo_dir}")
     subprocess.run(
         ["git", "-C", repo_dir, "pull", "--rebase"],
         check=True
     )
+    print("SUBPROCESS:  Repo successfully pulled.")
 
 def run_command(cmd, cwd):
-    print(f"Running: {cmd}")
+    print(f"SUBPROCESS:  Running: {cmd}")
     result = subprocess.run(cmd, shell=True, cwd=cwd, capture_output=True, text=True)
     if result.returncode != 0:
-        print(f"Error: {result.stderr}")
-        raise Exception(f"Command failed: {cmd}")
-    print(result.stdout)
+        print(f"SUBPROCESS:  Error: {result.stderr}")
+        raise Exception(f"SUBPROCESS:  Command failed: {cmd}")
+    print(f"SUBPROCESS:  {result.stdout}")
