4f0e7209a6027518089fa419cec4f79a0e74fdf8
adamivangrgic <grgic.ivan.adam@gmail.com>
2025-10-25 18:13:09 +0200

    subprocess fix

--- subprocess_functions.py
+++ subprocess_functions.py
@@ -5,6 +5,7 @@ def get_remote_hash(repo_url, branch='main'):
     print(f"SUBPROCESS: Getting {repo_url} {branch} hash")
     result = subprocess.check_output(
         ["git", "ls-remote", repo_url, f"refs/heads/{branch}"],
+        check=True,
         text=True
     )
     return result.split()[0] if result else None
@@ -14,7 +15,8 @@ def clone_repo(repo_url, repo_dir, branch="main"):
     if not os.path.exists(os.path.join(repo_dir, ".git")):
         subprocess.run(
             ["git", "clone", "--branch", branch, "--single-branch", repo_url, repo_dir],
-            check=True
+            check=True, 
+            timeout=600
         )
         print("SUBPROCESS: Repo successfully cloned.")
     else:
@@ -31,7 +33,7 @@ def pull_repo(repo_dir):
 
 def run_command(cmd, cwd):
     print(f"SUBPROCESS: Running: {cmd}")
-    result = subprocess.run(cmd, shell=True, cwd=cwd, capture_output=True, text=True)
+    result = subprocess.run(cmd, shell=True, cwd=cwd, capture_output=True, check=True, text=True, timeout=600)
     if result.returncode != 0:
         print(f"SUBPROCESS: Error: {result.stderr}")
         raise Exception(f"SUBPROCESS: Command failed: {cmd}")
