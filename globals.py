c902828849e86fd38e97fa63bd9f79a85678030a
adamivangrgic <grgic.ivan.adam@gmail.com>
2025-10-26 22:37:39 +0100

    logging file read write func

--- globals.py
+++ globals.py
@@ -17,6 +17,7 @@ def read_yaml_file(file_path: str) -> Dict[str, Any]:
     try:
         with open(file_path, 'r') as file:
             data = yaml.safe_load(file)
+        print(f"YAML read: {data}")
         return data
     except:
         print("YAML read: failed")
@@ -25,7 +26,9 @@ def read_yaml_file(file_path: str) -> Dict[str, Any]:
 def read_json_file(file_path: str) -> Dict[str, Any]:
     try:
         with open(file_path, 'r') as file:
-            return json.load(file)
+            data = json.load(file)
+        print(f"JSON read: {data}")
+        return data
     except:
         print("JSON read: failed")
         return None
@@ -34,6 +37,7 @@ def write_json_file(file_path: str, data):
     try:
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=2)
+        print(f"JSON write: {data}")
         return True
     except:
         print("JSON write: failed")
