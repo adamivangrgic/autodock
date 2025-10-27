8bc28405522a849b299c3c759228a13f2b6a9a0d
adamivangrgic <grgic.ivan.adam@gmail.com>
2025-10-27 21:37:31 +0100

    humanize dates fix

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -26,11 +26,3 @@ function humanizeDate(dateString) {
         return rtf.format(diffInDays, 'day');
     }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('[data-date]').forEach(element => {
-        const humanized = humanizeDate(element.getAttribute('data-date'));
-        element.textContent = humanized;
-        console.log(humanized, element.textContent)
-    })
-});
