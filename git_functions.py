09a22c7b99c1d9e45d614a263c9c9a86cd702729
adamivangrgic <grgic.ivan.adam@gmail.com>
2025-10-27 21:51:23 +0100

    log fix

--- git_functions.py
+++ git_functions.py
@@ -8,19 +8,19 @@ from subprocess_functions import get_remote_hash, clone_repo, pull_repo, run_com
 def git_clone(name: str, url: str, branch: str):
     repo_dir = os.path.join(globals.REPO_DATA_PATH, name)
 
-    print(f"TASK ({name}) : first instance, cloning repository.")
+    print(f"TASK ({name}) : cloning repository.")
     clone_repo(url, repo_dir, branch)
 
 
 def git_pull(name: str):
     repo_dir = os.path.join(globals.REPO_DATA_PATH, name)
 
-    print(f"TASK ({name}) : first instance, cloning repository.")
+    print(f"TASK ({name}) : pulling repository.")
     pull_repo(repo_dir)
 
 
 def git_check(name: str, url: str, branch: str, build_command: str, deploy_command: str):
-    print(f"TASK ({name}) : running git check task")
+    print(f"TASK ({name}) : running git check task.")
 
     if name not in globals.repo_data:
         globals.repo_data[name] = {
